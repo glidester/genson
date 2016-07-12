31ee78c745071e0c6c4194086d3a70b0a23bea79
Tim Wilkins <glidester@gmail.com>
2016-07-12 09:16:11 +0100

    Adding example of filtering property by type class

--- genson-scala/src/test/scala/com/owlike/genson/ScalaPropertyFilteringTest.scala
+++ genson-scala/src/test/scala/com/owlike/genson/ScalaPropertyFilteringTest.scala
@@ -1,5 +1,6 @@
 package com.owlike.genson
 
+import com.owlike.genson.reflect.RenamingPropertyNameResolver
 import org.scalatest.{FunSuite, Matchers}
 
 case class TestEntity(name: String, date: Long, amount: Int)
@@ -7,7 +8,7 @@ case class TestEntity2(name: String, date: Long, amount: Int)
 
 class ScalaPropertyFilteringTest extends FunSuite with Matchers {
 
-  test("test filtering out a property across all classes") {
+  test("test filtering out a property across all classes by name") {
     val genson = new ScalaGenson(new GensonBuilder().withBundle(ScalaBundle()).exclude("amount").create())
     val expected = """{"date":100,"name":"Hi"}"""
 
@@ -15,18 +16,25 @@ class ScalaPropertyFilteringTest extends FunSuite with Matchers {
     genson.toJson( TestEntity2("Hi",100L,10) ) should equal(expected)
   }
 
-  test("test filtering out a property for specific class") {
+  test("test filtering out a property for specific class by name") {
     val genson = new ScalaGenson(new GensonBuilder().withBundle(ScalaBundle()).exclude("amount",classOf[TestEntity]).create())
 
     genson.toJson( TestEntity("Hi",100L,10) ) should equal("""{"date":100,"name":"Hi"}""")
     genson.toJson( TestEntity2("Hi",100L,10) ) should equal("""{"amount":10,"date":100,"name":"Hi"}""")
   }
 
+  test("test filtering out a property across all classes by type") {
+    val genson = new ScalaGenson(new GensonBuilder().withBundle(ScalaBundle()).exclude(classOf[String]).create())
+
+    genson.toJson( TestEntity("Hi",100L,10) ) should equal("""{"amount":10,"date":100}""")
+  }
+
+
   /* !!!FAILS!!!!
 
   test("test renaming a property across all classes") {
-    val genson = new ScalaGenson(new GensonBuilder().withBundle(ScalaBundle()).rename("amount","quantity").create())
-    val expected = """{"quantity":10,"date":100,"name":"Hi"}"""
+    val genson = new ScalaGenson(new GensonBuilder().withBundle(ScalaBundle()).`with`(new RenamingPropertyNameResolver("name", null, classOf[String], "yourName")).create())
+    val expected = """{"amount":10,"date":100,"yourName":"Hi"}"""
 
     genson.toJson( TestEntity("Hi",100L,10) ) should equal(expected)
     genson.toJson( TestEntity2("Hi",100L,10) ) should equal(expected)
